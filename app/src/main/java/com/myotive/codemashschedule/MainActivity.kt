25489a7943cb4e0fa607b3738a0ff934cfd4d96a
ryan.carroll <carror6@nationwide.com>
2018-01-10 11:07:50 -0500

    adding first changes to exercise

--- app/src/main/java/com/myotive/codemashschedule/MainActivity.kt
+++ app/src/main/java/com/myotive/codemashschedule/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.myotive.codemashschedule
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btn_next_activity.setOnClickListener {
+            val intent = Intent(this, SessionDetailActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/myotive/codemashschedule/SessionDetailActivity.kt
@@ -0,0 +1,29 @@
+package com.myotive.codemashschedule
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+
+/**
+ * Created by carror6 on 1/10/2018.
+ */
+class SessionDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_speaker_detail)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // handle arrow click here
+        if (item.itemId == android.R.id.home) {
+            finish() // close this activity and return to preview activity (if there is any)
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+}
